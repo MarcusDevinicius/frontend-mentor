86cddb49a812ad412e19c0b5583cbe9229a4a016
MarcusDevinicius <163601890+MarcusDevinicius@users.noreply.github.com>
2024-10-24 18:42:50 -0300

    tentando resolver o B.O  do menu

--- interacoes.js
+++ interacoes.js
@@ -1,5 +1,6 @@
 var menuOut = document.getElementById('menuout');
 var burguer = document.getElementById('burguer')
+var btnX = document.getElementById('btn-x')
 
 function openMenu() {
     if (menuOut.style.display == 'none') {
@@ -17,3 +18,13 @@ function closeMenu() {
     }
 }
 
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menuOut.style.display = 'none'
+        burguer.style.display = 'none'
+    } if (window.innerWidth < 768 && btnX.style.display == 'none') {
+        burguer.style.display = 'block'
+    } if (window.innerWidth < 768){
+        burguer.style.display = 'block'
+    }
+}
