6e6afbe0dceaf805d9799b272138def6192d37b8
MarcusDevinicius <163601890+MarcusDevinicius@users.noreply.github.com>
2024-10-24 13:10:13 -0300

    treinando

--- interacoes.js
+++ interacoes.js
@@ -1,1 +1,19 @@
-var menuOut = document.getElementById('menuout')
+var menuOut = document.getElementById('menuout');
+var burguer = document.getElementById('burguer')
+
+function openMenu() {
+    if (menuOut.style.display == 'none') {
+        menuOut.style.display = 'block'
+        burguer.style.display = 'none'
+    } else {
+        menuOut.style.display = 'none'
+    }
+}
+
+function closeMenu() {
+    if (menuOut.style.display == 'block') {
+        menuOut.style.display = 'none'
+        burguer.style.display = 'block'
+    }
+}
+
